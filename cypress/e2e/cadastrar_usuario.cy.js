5be5e6d3609ab833fdfd88b06e1585eb8267423f
Thales Oliveira <TOliveiraDev@outlook.com>
2024-12-15 17:55:31 -0300

    Atualizar seletores do script teste de cadsatro de usuário

--- cypress/e2e/cadastrar_usuario.cy.js
+++ cypress/e2e/cadastrar_usuario.cy.js
@@ -1,11 +1,12 @@
 describe('Página de cadastro', () => {
   it('Preencher os campos do formulário corretamente para cadastrar um novo usuário', () => {
     cy.visit('https://adopet-frontend-cypress.vercel.app/');
-    cy.contains('a', "Cadastrar").click();
-    cy.get('input[name="nome"]').type("Thales Oliveira");
-    cy.get('input[name="email"]').type("umtesteai@email.com");
-    cy.get('input[name="password"]').type("Senha123");
-    cy.get('input[name="confirm_password"]').type("Senha123");
-    cy.contains('button', 'Cadastrar').click();
+    cy.get('[data-test="register-button"]').click();
+    cy.get('[data-test="input-name"]').type("Thales Oliveira");
+    cy.get('[data-test="input-email"]').type("umtesteai@email.com");
+    cy.get('[data-test="input-password"]').type("Senha123");
+    cy.get('[data-test="input-confirm-password"]').type("Senha123");
+    cy.get('[data-test="submit-button"]').click();
   })
 })
+
