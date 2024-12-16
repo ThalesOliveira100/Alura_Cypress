4436aa81b16609121a02ab6ea516221dc0671c3c
Thales Oliveira <TOliveiraDev@outlook.com>
2024-12-16 20:39:43 -0300

    Refatorar script login-incorreto com uso do comando personalisado 'login'

--- cypress/e2e/login-incorreto.cy.js
+++ cypress/e2e/login-incorreto.cy.js
@@ -5,9 +5,7 @@ describe('Falha no login do sistema', () => {
     })
 
     it('Deve preencher os campos do login incorretamente e o sistema deve exibe mensagens de erro específicas para o e-mail e senha inseridos na página', () => {
-        cy.get('[data-test="input-loginEmail"]').type('ana@emnail');
-        cy.get('[data-test="input-loginPassword"]').type('senh');
-        cy.get('[data-test="submit-button"]').click();
+        cy.login('ana@emnail', 'senh');
         cy.contains("Por favor, verifique o email digitado").should('be.visible')
         cy.contains("A senha deve conter pelo menos uma letra maiúscula, um número e ter entre 6 e 15 caracteres").should('be.visible')
     });
