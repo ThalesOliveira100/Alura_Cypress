4baa220e424513b120b5a3c64c54862921bbd808
Thales Oliveira <TOliveiraDev@outlook.com>
2024-12-15 22:07:03 -0300

    Configurar o cypress para rodar no cypress cloud. Comando: [npx cypress run --record --key d97b5a78-4d87-4cf9-a915-16ddb00749c2]

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "mjuk27",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
