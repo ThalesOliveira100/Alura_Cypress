59ce68426ce0f3e253056b27923e6e61917e7c7e
Thales Oliveira <TOliveiraDev@outlook.com>
2024-12-15 21:20:54 -0300

    Adicionar fluxo alternativo ao script de teste de login correto

--- cypress/e2e/login-correto.cy.js
+++ cypress/e2e/login-correto.cy.js
@@ -10,3 +10,16 @@ describe('Página de login', () => {
         cy.get('[data-test="submit-button"]').click();
     });
 });
+
+describe('Login correto fluxo alternativo', () => {
+    beforeEach(() => {
+        cy.visit('https://adopet-frontend-cypress.vercel.app/');
+    });
+
+    it('Acessa a homepage, clica no botão de ícone de mensagem no header e escreva nome e senha válidos na página de login.', () => {
+        cy.get('.header__message').click();
+        cy.get('[data-test="input-loginEmail"]').type('ana@email.com');
+        cy.get('[data-test="input-loginPassword"]').type('Senha123');
+        cy.get('[data-test="submit-button"]').click();
+    });
+});
