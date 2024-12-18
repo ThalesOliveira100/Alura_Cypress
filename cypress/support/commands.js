8a27f7e1751fe250157930669a3646067f138696
Thales Oliveira <TOliveiraDev@outlook.com>
2024-12-18 20:36:52 -0300

    Adicionar metodo .clear() no comando de login, para utilizar em testes massivos

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,16 +1,16 @@
 Cypress.Commands.add('login', (email, password) => { 
-    cy.get('[data-test="input-loginEmail"]').type(email);
-    cy.get('[data-test="input-loginPassword"]').type(password);
-    cy.get('[data-test="submit-button"]').click();
- })
+   cy.get('[data-test="input-loginEmail"]').clear().type(email);
+   cy.get('[data-test="input-loginPassword"]').clear().type(password);
+   cy.get('[data-test="submit-button"]').click();
+})
 
- Cypress.Commands.add('cadastrar', (nome, email, password) => {
-    cy.get('[data-test="input-name"]').type(nome);
-    cy.get('[data-test="input-email"]').type(email);
-    cy.get('[data-test="input-password"]').type(password);
-    cy.get('[data-test="input-confirm-password"]').type(password);
-    cy.get('[data-test="submit-button"]').click();
- })
+Cypress.Commands.add('cadastrar', (nome, email, password) => {
+   cy.get('[data-test="input-name"]').type(nome);
+   cy.get('[data-test="input-email"]').type(email);
+   cy.get('[data-test="input-password"]').type(password);
+   cy.get('[data-test="input-confirm-password"]').type(password);
+   cy.get('[data-test="submit-button"]').click();
+})
 
 // ***********************************************
 // This example commands.js shows you how to
