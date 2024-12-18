9f31810b043756d573572226269e66eb04ef8d2e
Thales Oliveira <TOliveiraDev@outlook.com>
2024-12-17 21:42:50 -0300

    Criar script para testes na api de mensagens

---
+++ cypress/e2e/api-mensagens.cy.js
@@ -0,0 +1,15 @@
+describe('API Adopet', () => {
+    const authorization = `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMTY0M2NkNi03MTEyLTQxNWItOTVkMi0wNzkwNGIwZDFhMWMiLCJhZG9wdGVyTmFtZSI6IkFuYSBkZSBKZXN1cyIsImlhdCI6MTczNDQ4MTk0MCwiZXhwIjoxNzM0NzQxMTQwfQ.nyhdEWCs-VHtS6DqUb3uI3hef-iXti4060R16Q6nwSQ`
+
+    it('Mensagens da API', () => {
+        cy.request({
+            method: 'GET',
+            url: 'https://adopet-api-i8qu.onrender.com/mensagem/11643cd6-7112-415b-95d2-07904b0d1a1c',
+            headers: { authorization }
+        }).then((res) => {
+            expect(res.status).to.be.equal(200)
+            expect(res.body).is.not.empty
+            expect(res.body).to.have.property('msg')
+        })
+    })
+})
